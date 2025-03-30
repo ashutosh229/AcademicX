a2f0c7a96e2260416f3f8839e3d0b982efa0a4bf
ashutosh229 <akumarjha875@gmail.com>
2025-03-30 20:32:11 +0530

    resolved the issue for the feedback form

--- frontend/app/courses/[id]/feedback/page.tsx
+++ frontend/app/courses/[id]/feedback/page.tsx
@@ -2,28 +2,28 @@
 
 import { setActiveCourseId } from "@/redux/slices/courseSlice";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import FeedbackForm from "./feedback-form";
 
-// // This function tells Next.js which paths to pre-render
-// export function generateStaticParams() {
-//   return courses.map((course) => ({
-//     id: course.course_id,
-//   }));
-// }
-
-export default function FeedbackPage({ params }: { params: { id: number } }) {
+export default function FeedbackPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  // Unwrap the params promise using use()
+  const { id } = use(params);
+
   useEffect(() => {
-    if (params.id) {
-      dispatch(setActiveCourseId(params.id));
+    if (id) {
+      dispatch(setActiveCourseId(Number(id))); // Ensure `id` is a number
     } else {
       router.push("/courses");
     }
-  }, [params.id, dispatch, router]);
+  }, [id, dispatch, router]);
 
   return <FeedbackForm />;
 }
