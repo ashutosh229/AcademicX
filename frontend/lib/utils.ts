2c6c48ed6c196714b3ebc9c0c88cfb31b25232ac
ashutosh229 <akumarjha875@gmail.com>
2025-03-30 22:18:05 +0530

    added the string utility function in utils

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatString(str: string) {
+  return str.includes("_")
+    ? str.replace(/_/g, " ").replace(/\b\w/g, (char) => char.toUpperCase())
+    : str.charAt(0).toUpperCase() + str.slice(1);
+}
