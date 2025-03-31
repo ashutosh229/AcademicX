766caa6f4d9e40356e328e897c3d0decbfa21118
ashutosh229 <akumarjha875@gmail.com>
2025-03-31 17:20:14 +0530

    modified the page.tsx of /profile/edit route

--- frontend/app/profile/edit/page.tsx
+++ frontend/app/profile/edit/page.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { getUserByEmail } from "@/lib/auth-utils";
+import { useFetchStudentProfile } from "@/hooks/custom-hooks/useFetchStudentProfile";
 import { RootState } from "@/redux/store";
-import { useSession } from "next-auth/react";
 import { useSelector } from "react-redux";
 import EditProfileForm from "./edit-profile-form";
 
 export default function EditProfilePage() {
-  const { data: session } = useSession();
-  const { students } = useSelector((state: RootState) => state.student);
+  const { activeStudent } = useSelector((state: RootState) => state.student);
 
-  const student = session?.user?.email
-    ? getUserByEmail(session.user.email, session, students)
-    : null;
+  useFetchStudentProfile();
 
-  if (!student) {
+  if (!activeStudent) {
     return (
       <div className="container mx-auto px-4 py-8">
         <div className="text-center">
@@ -25,31 +21,11 @@ export default function EditProfilePage() {
         </div>
       </div>
     );
-  } else if ("error" in student) {
-    console.log("Internal server error occured");
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-red-600">Access Denied</h1>
-          <p className="text-gray-600 mt-2">Internal server error occured</p>
-        </div>
-      </div>
-    );
-  } else if ("role" in student) {
-    console.log("Viewer access is given");
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-red-600">Access Denied</h1>
-          <p className="text-gray-600 mt-2">Viewers cannot access this page</p>
-        </div>
-      </div>
-    );
   } else {
     return (
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-2xl mx-auto">
-          <EditProfileForm student={student} />
+          <EditProfileForm student={activeStudent} />
         </div>
       </div>
     );
