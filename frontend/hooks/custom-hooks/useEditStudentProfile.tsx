cb91fa0b2e52ae234ebecea9a203df6d2b0daea7
ashutosh229 <akumarjha875@gmail.com>
2025-03-31 17:37:23 +0530

    added the edit student profile

---
+++ frontend/hooks/custom-hooks/useEditStudentProfile.tsx
@@ -0,0 +1,48 @@
+import { setError, setLoading } from "@/redux/slices/studentSlice";
+import { backendDomain } from "@/types/types";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "sonner";
+
+export function useEditProfile() {
+  const { data: session } = useSession();
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [success, setSuccess] = useState(false);
+
+  const handleEditProfile = async (name: string) => {
+    dispatch(setLoading(true));
+    setSuccess(false);
+
+    try {
+      const response = await fetch(`${backendDomain}/edit_student_name/`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: session?.user.email?.toString(),
+          name,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Unable to update the student's name");
+      }
+
+      toast.success("Updated the name of the user successfully");
+      setSuccess(true);
+      router.push("/profile");
+    } catch (error: any) {
+      console.error(error);
+      dispatch(setError(error.message));
+      toast.error(error.message);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  return { handleEditProfile, success };
+}
