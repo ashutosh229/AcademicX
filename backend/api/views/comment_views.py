27c88ea94caabe3ddaaf0794cf3ab72f4109e7ce
Shubham <shubhamy@iitbhilai.ac.in>
2025-04-03 08:54:17 +0530

    add comment & resource modified

--- backend/api/views/comment_views.py
+++ backend/api/views/comment_views.py
@@ -10,7 +10,7 @@ def add_comment(request):
     serializer = AddCommentSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response({"message": "comment added"}, status=201)
+        return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
 
 @api_view(['POST'])
