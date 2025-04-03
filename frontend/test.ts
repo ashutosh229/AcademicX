25cb7c193f052a351cfa8d70e9cd3894769007e8
ashutosh229 <akumarjha875@gmail.com>
2025-04-03 06:49:54 +0530

    push before tweaking the authentication system

--- frontend/test.ts
+++ frontend/test.ts
@@ -1,2 +1,3 @@
 console.log(process.env.NEXT_PUBLIC_BACKEND_DOMAIN)
 console.log("Pushed before optimizations and did not do optimizations")
+console.log("Pushed before tweaking the authentication system")
