70184bc68a53426672ae75db8b505e0c212924c5
ashutosh229 <akumarjha875@gmail.com>
2025-04-09 03:04:27 +0530

    pre-production commit: changed the next-auth types and tsconfig.json file

--- frontend/types/next-auth.d.ts
+++ frontend/types/next-auth.d.ts
@@ -5,7 +5,10 @@ import "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     user: {
+      name?: string;
+      email?: string;
       role?: string;
+      image?: string;
     } & DefaultSession["user"];
   }
 }
